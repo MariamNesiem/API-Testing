4d126be48ba801a88dcc8f801ba06c9a686e44cd
MariamNesiem <mariam.nesiem@yahoo.com>
2021-03-13 22:40:18 +0200

    add more projects

--- Projects/APIFramework/src/test/java/cucumber/Options/TestRunner.java
+++ Projects/APIFramework/src/test/java/cucumber/Options/TestRunner.java
@@ -6,9 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features",
-plugin ="json:target/jsonReports/cucumber-report.json",
-glue= {"steps"})
+@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"steps"})
 public class TestRunner {
 	//tags= {"@DeletePlace"}
 }
